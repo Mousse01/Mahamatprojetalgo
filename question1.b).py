087ab6bd7171644132e4c320d031506d240dd7bc
Mousse01 <121554844+Mousse01@users.noreply.github.com>
2023-04-10 15:12:39 +0100

    Add files via upload
    Question numéro 1 sous question a et b

---
+++ question1.b).py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 10 12:42:53 2023
+
+@author: gj
+"""
+
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+import numpy as np
+
+def ODE(x,t,k1, k2):
+    dxdt = [x[1],-k1*x[1]-k2*x[0]+ 40*np.cos(10*t)]
+    return dxdt
+
+# Initial condition
+x0 =[0.01, 0]
+
+timePoints = np.linspace(1, 10, 400)
+k1 = 2
+k2 = 400
+
+# we call the odeint() function
+solutionOde = odeint(ODE,x0,timePoints, args=(k1, k2))
+
+# plot solution
+plt.plot(timePoints, solutionOde[:, 0], 'b', label = 'x1')
+plt.plot(timePoints, solutionOde[:, 1], 'g', label = 'x2')
+plt.legend(loc='best')
+plt.xlabel('time')
+plt.ylabel('x1(t), x2(t)')
+plt.grid()
+plt.savefig('Simulation.png')
+plt.show()
